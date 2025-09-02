dac34bacebece53bcfff6ec5e1cfbcff8e5c95b3
塩見陵介 <ryosukesukairain1@gmail.com>
2025-09-02 18:21:57 +0900

    一部構成変更

--- lib/app.dart
+++ lib/app.dart
@@ -2,7 +2,7 @@ import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 
 import 'flavors.dart';
-import 'pages/my_home_page.dart';
+import 'my_home_page.dart';
 
 class App extends StatelessWidget {
   const App({super.key});
@@ -16,18 +16,19 @@ class App extends StatelessWidget {
     );
   }
 
-  Widget _flavorBanner({required Widget child, bool show = true}) => show
-      ? Banner(
-          location: BannerLocation.topStart,
-          message: F.name,
-          color: Colors.green.withAlpha(150),
-          textStyle: TextStyle(
-            fontWeight: FontWeight.w700,
-            fontSize: 12.0,
-            letterSpacing: 1.0,
-          ),
-          textDirection: TextDirection.ltr,
-          child: child,
-        )
-      : Container(child: child);
+  Widget _flavorBanner({required Widget child, bool show = true}) =>
+      show
+          ? Banner(
+            location: BannerLocation.topStart,
+            message: F.name,
+            color: Colors.green.withAlpha(150),
+            textStyle: TextStyle(
+              fontWeight: FontWeight.w700,
+              fontSize: 12.0,
+              letterSpacing: 1.0,
+            ),
+            textDirection: TextDirection.ltr,
+            child: child,
+          )
+          : Container(child: child);
 }
--- lib/my_home_page.dart
+++ lib/my_home_page.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import '../flavors.dart';
+import 'flavors.dart';
 
 class MyHomePage extends StatelessWidget {
   const MyHomePage({super.key});
